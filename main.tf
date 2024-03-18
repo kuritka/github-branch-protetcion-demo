b6239e276b98085b0a9ab46173a34ca09e2692bf
Michal Kuritka <kuritka@gmail.com>
2024-03-18 14:55:29 +0100

    TFLint

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 # https://registry.terraform.io/providers/integrations/github/latest/docs/resources/repository
 resource "github_repository" "github-branch-protetcion-demo-instance" {
-  name                 = "github-branch-protetcion-demo-instance"
-  description          = "created by github-branch-protetcion-demo 🟢"
+  name        = "github-branch-protetcion-demo-instance"
+  description = "created by github-branch-protetcion-demo 🟢"
+  #tfsec:ignore:github-repositories-private
   visibility           = "public" # this must be public otherwise 422 Secret Scanning can only be changed on org owned repositories []
   vulnerability_alerts = true
 }
 
 
 resource "github_branch_protection" "status_checks" {
-  repository_id = github_repository.github-branch-protetcion-demo-instance.id
-  pattern = "main"
-
+  repository_id          = github_repository.github-branch-protetcion-demo-instance.id
+  pattern                = "main"
+  require_signed_commits = true
   required_status_checks {
-    strict = true
-    contexts = ["GitLeaks", "YamlLint"]
+    strict   = true
+    contexts = ["GitLeaks", "YamlLint","TFLint"]
   }
 }
-
 
 #resource "github_branch_protection_v3" "example" {
 #  repository     = github_repository.github-branch-protetcion-demo-instance.name
--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,8 @@ output "url" {
   description = "full repository url"
   value       = github_repository.github-branch-protetcion-demo-instance.git_clone_url
 }
+
+output "protection_rules" {
+  description = "full repository url"
+  value       = github_branch_protection.status_checks.required_status_checks
+}
